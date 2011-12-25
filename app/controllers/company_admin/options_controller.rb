b9f1ba382558b3ab7a363042155d4a2022a201c3
David Henner <drhenner@yahoo.com>
2011-12-25 13:30:03 -0500

    looks better

--- app/controllers/company_admin/options_controller.rb
+++ app/controllers/company_admin/options_controller.rb
@@ -1,10 +1,18 @@
 class CompanyAdmin::OptionsController < ApplicationController
-  helper_method :company
+  helper_method :company, :option_selected, :collaborators_selected
   def index
     #
   end
 
   private
+    def option_selected
+      'selected'
+    end
+
+    def collaborators_selected
+      ''
+    end
+
     def company
       @company ||= current_user.companies.find(params[:company_id])
     end
